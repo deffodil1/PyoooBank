379a4ac07d5fb9bfb7ba9a5f6eec0ab52e12c397
unknown <deffodilkhan537@gmail.com>
2025-05-06 10:29:05 +0600

    3

--- Js/Home.js
+++ Js/Home.js
@@ -1,1 +1,7 @@
-console.log("Hello Home Mama");
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+    console.log('add money button is clicked');
+    const AddMoney = document.getElementById('input-add-money').value;
+    const InputPin = document.getElementById('input-pin').value;
+    console.log(AddMoney, InputPin);
+})
