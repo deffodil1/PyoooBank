d4ece1db9f0e8ecdbac981ccde6c5704b7863f5b
unknown <deffodilkhan537@gmail.com>
2025-05-06 10:54:35 +0600

    5

--- Js/Home.js
+++ Js/Home.js
@@ -3,5 +3,17 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     console.log('add money button is clicked');
     const AddMoney = document.getElementById('input-add-money').value;
     const InputPin = document.getElementById('input-pin').value;
-    console.log(AddMoney, InputPin);
+    const Balance = document.getElementById('balance').innerText;
+    const AddMoneyNumber = parseFloat(AddMoney);
+    const BalanceNumber = parseFloat(Balance);
+    const NewBalance = AddMoneyNumber + BalanceNumber;
+    document.getElementById('balance').innerText = NewBalance;
+    
+    
+    console.log( NewBalance);
+    if(InputPin === '1234'){
+        console.log('The money is added');
+    }else{
+        alert('failed to add');
+    }
 })
